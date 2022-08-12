7e895a159a4137648277fb4be5a80a784702b6cc
VegethYT <84522055+VegethYT@users.noreply.github.com>
2022-08-12 19:27:15 +0200

    Test

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -174,13 +174,13 @@ class MainMenuState extends MusicBeatState
 				}
 			}
 
-			if (FlxG.keys.justPressed.UP)
+			if (FlxG.keys.justPressed.UP_P)
 			{
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 				changeItem(-1);
 			}
 
-			if (FlxG.keys.justPressed.DOWN)
+			if (FlxG.keys.justPressed.DOWN_P)
 			{
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 				changeItem(1);
